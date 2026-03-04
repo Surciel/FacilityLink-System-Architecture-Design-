85cb3478cff9cf1cf01f111baf2254145dec78b6
Reiygn <reyadrianlouis914@gmail.com>
2026-03-04 17:01:49 +0800

    Add new files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import { useEffect, useState } from "react";
-import { supabase } from "./supabaseClient";
+import Categories from "./admin/Categories";
 
 function App() {
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      let { data: rows, error } = await supabase.from("my_table").select("*");
-      if (error) console.error(error);
-      else setData(rows);
-    };
-    fetchData();
-  }, []);
-
   return (
     <div>
-      <h1>Supabase Data</h1>
+      <h1>Supabase Datawiwiwiwwi</h1>
       <pre>{JSON.stringify(data, null, 2)}</pre>
+    <Categories />
     </div>
   );
+
 }
 
 export default App;
---
+++ src/admin/Stocks.js
@@ -0,0 +1,5 @@
+function Stocks() {
+  return <h1>Stocks Page</h1>;
+}
+
+export default Stocks;
