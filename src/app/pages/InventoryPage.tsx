d60326a63aeb70639b4b1221c5cb7babf407245f
Surciel <kenchiepgomezshjcs@gmail.com>
2026-05-24 05:28:18 +0800

    Fixed authentication and supabase requests

--- src/app/pages/InventoryPage.tsx
+++ src/app/pages/InventoryPage.tsx
@@ -1014,7 +1014,6 @@ export function InventoryPage() {
                     <option value="JMS">JMS</option>
                     <option value="GYM">GYM</option>
                   </select>
-                  <q></q>
                 </div>
                 <div>
                   <label className="block text-sm font-medium text-gray-700 mb-2">
